2717418d4af034d39809b0e92ab7eea4d9b959b8
Alexander Edinger <alexander.edinger1@gmail.com>
2024-11-30 20:44:53 +0100

    Playing with some components

--- App.tsx
+++ App.tsx
@@ -5,114 +5,83 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+	Button,
+	SafeAreaView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	TextInput,
+	useColorScheme,
+	View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+	Colors
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type MyOwnProps = {
+	initialState: boolean
+}
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const MyOwnComponent = (props: MyOwnProps) =>
+{
+	const [getActive, setActive] = useState(props.initialState);
+
+	return (
+		<View>
+		<Text selectable={getActive}>
+			{getActive ? "you can select me" : "you can select me NOT"}
+		</Text>
+		<Button
+			onPress={() => setActive(!getActive)}
+			title="switch the selectability"/>
+		</View>
+	)
 }
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+	const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+	const backgroundStyle = {
+	backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+	};
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+	return (
+	<SafeAreaView style={backgroundStyle}>
+		<StatusBar
+		barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+		backgroundColor={backgroundStyle.backgroundColor}
+		/>
+		<Text>
+			just some text
+		</Text>
+		<TextInput
+			placeholder="some text here"/>
+		<MyOwnComponent initialState={true}/>
+		<MyOwnComponent initialState={false}/>
+	</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+	sectionContainer: {
+	marginTop: 32,
+	paddingHorizontal: 24,
+	},
+	sectionTitle: {
+	fontSize: 24,
+	fontWeight: '600',
+	},
+	sectionDescription: {
+	marginTop: 8,
+	fontSize: 18,
+	fontWeight: '400',
+	},
+	highlight: {
+	fontWeight: '700',
+	},
 });
 
 export default App;
